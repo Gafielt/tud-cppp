8ba241835071c2da48f655fd12deb08f9342419a
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-08-24 11:10:21 +0200

    Add docu to USB_UART module

--- exercises/templates/WinIdeaWorkspaceTemplate/lib/uart_usb.h
+++ exercises/templates/WinIdeaWorkspaceTemplate/lib/uart_usb.h
@@ -5,7 +5,26 @@
 #include "pdl_header.h"
 #include <string.h>
 
-static char szUartTxBuf[] = "Welcome to Cypress\n\r"; 
+/*
+ * The following code is a minimal working example.
+ * 1. Create main.c with the following content
+#include "uart_usb.h"
+int main(){
+  initBoard();
+  UART_USB_Init();
+  UART_USB();
+  return 0;
+}
+ * 2. Install Cypress Serial Port Viewer from http://www.cypress.com/documentation/software-and-drivers/serial-port-viewer-and-terminal
+ * 3. Run the Serial Port Viewer
+ * 4. Adjust the baud rate to the value of Cppp_DefaultBaudRate
+ * 5. Press the Disconnected button to connect
+ * 6. Reset the evaluation board.
+ * 7. You should see a welcome message. Start typing and you will receive a copy of what you type in.
+*/
+
+static uint32_t Cppp_DefaultBaudRate = 115200u;
+static char szUartTxBuf[] = "Welcome to the C/C++ lab!\r\n"; 
 volatile stc_mfsn_uart_t* pstcUart0;
 stc_mfs_uart_config_t stcUartConfig; 
 uint8_t u8Count; 
--- exercises/templates/WinIdeaWorkspaceTemplate/src/main.c
+++ exercises/templates/WinIdeaWorkspaceTemplate/src/main.c
@@ -1,9 +1,13 @@
 #include "init.h"
 #include "blink.h"
 #include "blinkrainbow.h"
+#include "uart_usb.h"
 
 int main(){
   initBoard();
+  
+  UART_USB_Init();
+  UART_USB();
   
   // Write your code here ...
   BlinkMain();
