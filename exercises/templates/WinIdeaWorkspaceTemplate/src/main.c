e400b5bc8ac1392f4a7beacdf2ce681e00930a8d
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-09-11 14:43:56 +0200

    Save workspace and restore main.c
    [skip ci]

--- exercises/templates/WinIdeaWorkspaceTemplate/src/main.c
+++ exercises/templates/WinIdeaWorkspaceTemplate/src/main.c
@@ -1,12 +1,8 @@
 #include "init.h"
 #include "blink.h"
-#include "board_test.h"
 
 int main(){
   initBoard();
-  
-  cppp_initBoardTest();
-  cppp_boardTest();
 
   // Write your code here ...
   //BlinkMain();   
