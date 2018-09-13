dfb693441c326514f12e172acbf99b5572f5daf1
Roland Kluge <roland.kluge@es.tu-darmstadt.de>
2018-09-13 14:58:44 +0200

    Proofreading unit testing exercise

--- exercises/latex/04_advanced/problems/listings/unittests.cpp
+++ exercises/latex/04_advanced/problems/listings/unittests.cpp
@@ -4,26 +4,27 @@
 
 TEST(ListSimpleUsageTest)
 {
-	// teste appendElement und deleteFirst für List<int>
+	// Teste appendElement und deleteFirst für List<int>
+  // Wie sind die erwarteten Werte von getSize() und ggf. getFirstElement()?
 }
 
 TEST(ListAdvancedUsageTest) {
 	
-	// füge an eine List<double> Elemente an und an beliebieger Stelle eine
+	// Füge an eine List<double> Elemente an und an beliebieger Stelle eine
 	// prüfe,ob das n-te Element für jeden Eintrag der Erwartung entpricht
 }
 
 TEST(ListIteratorTest)
 {
-	// Nutze den Iterator um wortweise "Hello World!" einer Liste hinzuzufügen
+	// Nutze den Iterator um wortweise "Hello World!" einer Liste hinzuzufügen.
 }
 
 TEST(ExceptionsTest)
 {
-	// teste die 3 Methoden, bei denen Exceptions auftreten können
+	// Teste die 3 Methoden, bei denen Exceptions auftreten können.
 }
 
-int main(int,char **)
+int main(int, char **)
 {
 	return UnitTest::RunAllTests();
 }
